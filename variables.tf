19d9004116be3a5e9c7a1e67145fd2e3466f81fa
imamariefrahman <imam@email.unikom.ac.id>
2020-08-02 22:25:03 +0700

    adjusted to version 0.11

--- variables.tf
+++
@@ -1,7 +0,0 @@
-variable "profile" {
-  description = "AWS User account Profile"
-  default     = "staging"
-}
-variable "region" {
-  default = "us-east-2"
-}
--- vpc/output.tf
+++
@@ -1,8 +0,0 @@
-output "vpc_id" {
-  description = "The ID of the VPC"
-  value       = aws_vpc.this.id
-}
-
-output "nodes_subnet" {
-  value = aws_subnet.nodes_subnet.id
-}
